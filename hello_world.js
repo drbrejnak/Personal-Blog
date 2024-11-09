9146d97a2a812786d9031e5de716e9e1600cc840
drbrejnak <drbrejnak@gmail.com>
2024-11-09 15:47:52 -0500

    Header update with JS and Home page fade-in transition on load

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+var prevScrollpos = window.scrollY;
+
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("hnav").classList.remove("hidden");
+  } else {
+    document.getElementById("hnav").classList.add("hidden");
+  }
+  prevScrollpos = currentScrollPos;
+}
